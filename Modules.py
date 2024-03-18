335903ed66d5acf33ee6598e2191201c490c3b48
amanaarshad <41306680+amanaarshad@users.noreply.github.com>
2024-03-18 00:47:52 -0700

    Initial commit

---
+++ Modules.py
@@ -0,0 +1,13 @@
+# import module by renaming it
+import math as m
+
+print(m.pi)
+
+# Output: 3.141592653589793
+#-----------------------------------------------------------------------------------------------
+-# import all names from the standard module math
+from math import *
+
+print("The value of pi is", pi)
+#-----------------------------------------------------------------------------------------------
+
